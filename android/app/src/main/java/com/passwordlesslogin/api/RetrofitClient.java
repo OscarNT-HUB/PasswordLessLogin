f2cba58d515a5284c9e44be74a100ef0a430fa9d
Oscar Nuñez <jmoreno.fac@gmail.com>
2026-06-17 14:27:58 -0600

    Change API URL to Render deployment

--- android/app/src/main/java/com/passwordlesslogin/api/RetrofitClient.java
+++ android/app/src/main/java/com/passwordlesslogin/api/RetrofitClient.java
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
-    private static final String BASE_URL = "https://referred-millions-blah-drives.trycloudflare.com/";
+    private static final String BASE_URL = "https://passwordless-login-vc59.onrender.com/";
     private static Retrofit retrofit;
 
     public static Retrofit getInstance() {
